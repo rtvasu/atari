337d4c07edb79d1db21fb996e115d176ea9a3cf1
Aarti Vasudevan <aartidevan@gmail.com>
2025-12-14 02:51:15 -0500

    Reducing repetition, some design changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
 export default function Home() {
+  type FieldProps = {
+    label: string;
+    autoFocus?: boolean;
+  };
+
+  function Field({ label, autoFocus }: FieldProps) {
+    return (
+      <label className="max-w-xs flex justify-between">
+        {label}:
+        <input autoFocus={autoFocus} className="border border-foreground px-2" />
+      </label>
+    );
+  };
+  
   return (
-    <div className="flex flex-col">
-      <label>
-        Username: 
-        <input autoFocus className="border border-foreground px-2" />
-      </label>
-      <label>
-        Password: 
-        <input className="border border-foreground px-2" />
-      </label>
-      <label>
-        Phone: 
-        <input className="border border-foreground px-2" />
-      </label>
-      <button className="border border-foreground max-w-xs cursor-pointer bg-fuchsia-400 hover:bg-fuchsia-500">Sign Up</button>
+    <div className="flex flex-col gap-3 p-4">
+      <Field label="Email" autoFocus />
+      <Field label="Password" />
+      <Field label="Phone" />
+      <button className="border border-foreground max-w-xs cursor-pointer bg-fuchsia-300 hover:bg-fuchsia-400">Sign Up</button>
     </div>
   );
 }
