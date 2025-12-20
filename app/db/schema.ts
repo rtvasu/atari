2daebb5d20837e7e896dbbb4e4c0f1899388d2bb
Aarti Vasudevan <aartidevan@gmail.com>
2025-12-20 13:52:55 -0500

    Adding new table users, and associated migrations. Deleted old schema.ts

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,5 +1,13 @@
-import { pgTable, text } from "drizzle-orm/pg-core"
+import { pgTable, text, serial, timestamp } from "drizzle-orm/pg-core"
 import { sql } from "drizzle-orm"
 export const invites = pgTable("invites", {
-    email: text().primaryKey().notNull(),
+	email: text().primaryKey().notNull(),
+});
+
+export const users = pgTable("users", {
+	id: serial("id").primaryKey(),
+	username: text().notNull().unique(),
+	phone: text().notNull(),
+	createdAt: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
+	updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow().notNull(),
 });
--- drizzle/schema.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, text } from "drizzle-orm/pg-core"
-import { sql } from "drizzle-orm"
-
-
-
-export const invites = pgTable("invites", {
-	email: text().primaryKey().notNull(),
-});
