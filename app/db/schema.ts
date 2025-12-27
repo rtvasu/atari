f9d03550fab603734d466f5788e68dc254aba9e9
Aarti Vasudevan <aartidevan@gmail.com>
2025-12-27 03:23:56 -0500

    adding a session table to persist login through a token

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, serial, timestamp } from "drizzle-orm/pg-core"
+import { pgTable, text, serial, timestamp, uuid } from "drizzle-orm/pg-core"
 import { sql } from "drizzle-orm"
 export const invites = pgTable("invites", {
 	email: text().primaryKey().notNull(),
@@ -10,5 +10,14 @@ export const users = pgTable("users", {
 	phone: text().notNull(),
 	createdAt: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
 	updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow().notNull(),
-	passwordHash: text().notNull(),
+	passwordHash: text("password_hash").notNull(),
+});
+
+export const sessions = pgTable("sessions", {
+	id: uuid("id").defaultRandom().primaryKey(),
+	userid: serial("user_id").notNull().references(() => users.id, { onDelete: "cascade" }),
+	expiresAt: timestamp("expires_at", { withTimezone: true }).notNull(),
+	createdAt: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
+	revokedAt: timestamp("revoked_at", { withTimezone: true }).defaultNow().notNull(),
+	sessionTokenHash: text("session_token_hash").notNull(),
 });
